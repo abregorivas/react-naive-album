9db30dbf448fe78c7231c966146c6a182387a557
Roland Abregorivas <abregorivas@gmail.com>
2017-08-14 15:26:24 -0700

    refactor index prep for auth logic

---
+++ src/components/Albums.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {View } from 'react-native'
+
+import Header from './Header'
+import AlbumList from './AlbumList'
+
+class Albums extends Component {
+  render() {
+    return (
+      <View style={ {flex: 1} }>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+          );
+        }
+      }
+
+      export default Albums
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {View } from 'react-native'
+
+//Components
+import Albums from './Albums'
+
+class App extends Component {
+  render () {
+    return (
+      <Albums />
+    )
+  }
+}
+
+export default App
