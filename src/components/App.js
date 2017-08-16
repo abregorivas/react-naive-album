e1cca80167a81ce6447af7a84ed3f966198212a9
~A <arayamajhi@endertech.com>
2017-08-16 12:02:03 -0700

    Branch initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import firebase from 'firebase'
 
 //Components
@@ -54,6 +54,7 @@ class App extends Component {
   render () {
     return (
       <View>
+          <Text>GI CLONED</Text>
           {this.renderContent()}
       </View>
     )
