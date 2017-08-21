acaef6c61c2e85ccfac993117745bb91643c6cbb
Roland Abregorivas <abregorivas@gmail.com>
2017-08-20 21:49:55 -0700

    refactored artistCreate split to artistForm

--- src/components/ArtistCreate.js
+++ src/components/ArtistCreate.js
@@ -1,64 +1,27 @@
 import React, { Component } from 'react'
-import { Picker, Text, View } from 'react-native'
-import { Card, CardItem, Input, Button } from './common'
+import {  View } from 'react-native'
+import { Card, CardItem, Button } from './common'
 import { connect } from 'react-redux'
 import { artistUpdate, artistCreate } from '../actions'
-import firebase from 'firebase'
+import ArtistForm from './ArtistForm'
 
 class ArtistCreate extends Component {
+
   OnButtonPress() {
     const {name, phone, genre } = this.props
     this.props.artistCreate({name, phone, genre: genre || 'Rock' })
   }
+
   render () {
     console.log('Artistprops', this.props.artist)
     return (
       <Card>
-        <CardItem>
-          <Input
-          label="Name"
-          placeholder="Artist Name"
-          value={this.props.name}
-          onChangeText={(value) => this.props.artistUpdate({prop: 'name', value })}
-          />
-        </CardItem>
-
-        <CardItem>
-          <Input
-          label="Phone"
-          placeholder="555-555-5555"
-          value={this.props.phone}
-          onChangeText={(value) => this.props.artistUpdate({prop: 'phone', value })}
-          />
-        </CardItem>
-
-        <CardItem style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerLabelStyle}>Genre</Text>
-          <Picker
-          selectedValue={this.props.genre}
-          onValueChange={value => this.props.artistUpdate({prop: 'genre', value})}
-          >
-              <Picker.Item label="Rock" value="Rock" />
-              <Picker.Item label="Country" value="Country" />
-              <Picker.Item label="Folk" value="Folk" />
-              <Picker.Item label="Pop" value="Pop" />
-              <Picker.Item label="Clasical" value="Clasical" />
-              <Picker.Item label="Rap" value="Rap" />
-          </Picker>
-        </CardItem>
-<Text/>
+        <ArtistForm {...this.props}/>
         <CardItem>
           <Button onPress={this.OnButtonPress.bind(this)}>Save</Button>
         </CardItem>
       </Card>
     )
-  }
-}
-
-const styles = {
-  pickerLabelStyle: {
-    fontSize: 18,
-    paddingLeft: 20
   }
 }
 
